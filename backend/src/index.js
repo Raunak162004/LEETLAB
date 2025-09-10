e6fff4d765c3e921e53a8b74f01d87e0c499ad19
Raunak Malhotra <rm1273503@gmail.com>
2025-09-11 00:10:48 +0530

    updated index.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,11 +8,19 @@ const app = express();
 // routes
 import authRoutes from "./routes/auth.routes.js"
 import problemRoutes from './routes/problem.routes.js';
+import executionRoutes from './routes/executeCode.route.js';
+import healthcheckRoutes from './routes/healthcheck.route.js';
+import playlistRoutes from './routes/playlist.routes.js';
+import submissionRoutes from './routes/submission.routes.js';
 
 app.use(express.json());
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/problems', problemRoutes);
+app.use('/api/v1/execute', executionRoutes);
+app.use('/api/v1/healthcheck', healthcheckRoutes);
+app.use('/api/v1/playlists', playlistRoutes);
+app.use('/api/v1/submissions', submissionRoutes);
 
 const PORT = process.env.PORT || 3000;
 
