c21c70d63af8b92713b9848d671c87c44f900e11
Raunak Malhotra <rm1273503@gmail.com>
2025-09-10 23:59:54 +0530

    updated auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
+import { authMiddleware } from "../middleware/auth.middleware.js";
 import express from "express";
-import { check, login, logout, register } from "../controllers/auth.controller.js";
-
+import { register,login,logout,check,forgetPassword,changeRole } from "../controllers/auth.controller.js";    
 const authRoutes = express.Router();
 
-authRoutes.post("/register", register)
-authRoutes.post("/login", login)
-authRoutes.post("/logout", logout)
-authRoutes.get("/check", check)
+authRoutes.post("/register", register);
+authRoutes.post("/login", login);
+authRoutes.post("/logout",authMiddleware,logout);
+authRoutes.get("/check", authMiddleware,check);
+authRoutes.post("/forgot-password",authMiddleware,forgetPassword);
+authRoutes.post("/change-role",authMiddleware,changeRole);
 
-export default authRoutes;
+export default authRoutes
